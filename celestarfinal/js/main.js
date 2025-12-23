b76bd267c9c510e8fcc4d043f8aad7b1b0413d7c
董庭婕 <s111319020@stu.ntue.edu.tw>
2025-12-23 18:03:52 +0800

    Update main.js

--- celestarfinal/js/main.js
+++ celestarfinal/js/main.js
@@ -78,7 +78,7 @@ function renderProducts() {
               <img src="${imgSrc}" alt="${p.name}">
             </div>
             <div class="product-name">${p.name}</div>
-            <div class="product-price">$${p.price}</div>
+            <div class="product-price">${p.price}</div>
           </a>
         `;
         grid.appendChild(card);
@@ -305,6 +305,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
 
 
 
