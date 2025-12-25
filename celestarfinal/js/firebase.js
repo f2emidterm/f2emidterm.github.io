e249eef4bf4e474b7b847f701c0413b63b1b8eb4
董庭婕 <s111319020@stu.ntue.edu.tw>
2025-12-25 14:10:59 +0800

    Update firebase.js

--- celestarfinal/js/firebase.js
+++ celestarfinal/js/firebase.js
@@ -1,18 +1,5 @@
 // js/firebase.js
 
-// 改用網址引入 (CDN)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBp1JpKRDAi0aJ-0-jlwyqP9uPQbbxzdOA",
-    authDomain: "f2efinal-9e6f5.firebaseapp.com",
-    projectId: "f2efinal-9e6f5",
-    storageBucket: "f2efinal-9e6f5.firebasestorage.app",
-    messagingSenderId: "188709687038",
-    appId: "1:188709687038:web:175b00ef902418a33e5faf"
-};
-
 // 1. 引入必要的功能
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 // ★ 缺漏點 1：你必須引入 getAuth 和 getFirestore 才能建立變數
@@ -54,5 +41,6 @@ const db = getFirestore(app);
 // 重要：一定要 export db，這樣 main.js 才拿得到
 
 export { auth,db };
+
 
 
